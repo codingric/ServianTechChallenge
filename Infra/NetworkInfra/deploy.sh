6e4bcaeca8bd9eda1a1f867e78188ed2cfc57351
amit.erande <amit.erande@sourcedgroup.com>
2021-10-14 16:26:46 +1100

    rearranged the git directory structure

---
+++ Infra/NetworkInfra/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+Environment=$1
+export Environment=$1
+
+ROLE="arn:aws:iam::852998739144:role/teamAdmin"
+
+KST=(`aws --profile amit-teamAdmin sts assume-role --role-arn $ROLE --role-session-name key-rotation --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' --output text`)
+
+unset AWS_SECURITY_TOKEN
+export AWS_ACCESS_KEY_ID=${KST[0]}
+export AWS_SECRET_ACCESS_KEY=${KST[1]}
+export AWS_SESSION_TOKEN=${KST[2]}
+export AWS_SECURITY_TOKEN=${KST[2]}
+
+
+ansible-playbook NetworkInfra-ansible.yml -e "EnvPrefix=$Environment"
+# export PROJECTACCOUNTS="654996818359 094551496269"
+# python3 test_python.py
