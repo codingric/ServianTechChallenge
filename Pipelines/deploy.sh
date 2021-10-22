163d348cb781a1c41399966f103654d8ee104e17
amit.erande <amit.erande@sourcedgroup.com>
2021-10-22 12:00:09 +1100

    Updated deploy.sh with helpfunction

--- Pipelines/deploy.sh
+++ Pipelines/deploy.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
-Environment=$1
-export Environment=$1
 
-ROLE="arn:aws:iam::852998739144:role/teamAdmin"
+helpfunction ()
+{
+printf "\nUsage: deploy.sh -e NonProd -p non-prodTeamAdmin -r RoleName \n\n-e = Environment name to deploy the stack. Valid values are NonProd, Prod.\n
+-p = Local AWS CLI profile to use to connect to AWS account.\n
+-r = Role name to be assumed while deploying the stack.\n"
+exit 1
+}
 
-KST=(`aws --profile amit-teamAdmin sts assume-role --role-arn $ROLE --role-session-name manual-ansible-deployment --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' --output text`)
+
+while getopts e:p:r: flag
+do
+    case "${flag}" in
+        e) Environment=${OPTARG};;
+        p) Profile=${OPTARG};;
+        r) Role=${OPTARG};;
+        ?) helpfunction ;;
+    esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$Environment" ] || [ -z "$Profile" ] || [ -z "$Role" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+AWS_ACCOUNT=(`aws sts get-caller-identity --query Account --output text`)
+ROLE="arn:aws:iam::${AWS_ACCOUNT}:role/${Role}"
+
+KST=(`aws --profile ${Profile} sts assume-role --role-arn $ROLE --role-session-name manual-ansible-deployment-${Role} --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' --output text`)
 
 unset AWS_SECURITY_TOKEN
 export AWS_ACCESS_KEY_ID=${KST[0]}
